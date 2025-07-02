9b545c1ad2c4559969e27ce36afbed003a637894
Alex <amvvec@gmail.com>
2025-07-02 16:24:02 +0300

    add: FAIL and CHECK macros refactor: EXPECT_TRUE and ASSERT_TRUE with new FAIL and CHECK macros

--- include/unit/macros.h
+++ include/unit/macros.h
@@ -4,6 +4,26 @@
 
 #include <iostream>
 
+// VERY GOOD IDEA
+// ✅ Можешь добавить SFINAE или static_assert (или оставить — GTest тоже не решает это полностью)
+
+// TODO: refactor all macroses
+#define FAIL(message) \
+    do \
+    { \
+        std::cerr << "[ FAIL ] " << __FILE__ << ":" << __LINE__ << ": " << message << std::endl; \
+        if(unit::current_test_failed) *unit::current_test_failed = true; \
+    } while(0)
+
+#define CHECK(condition, message, should_return) \
+    do \
+    { \
+        if(!condition) { \
+            FAIL(message); \
+            if(return) should_return; \
+        } \
+    }
+
 #define TEST(group, name) \
     static void test_##group##_##name(); \
     static unit::AutoRegister auto_##group##_##name(#group, #name, &test_##group##_##name); \
@@ -43,6 +63,9 @@
     ASSERT_TRUE((mock).is_called())
 
 // Standard macros
+#define EXPECT_TRUE(condition) CHECK(condition, "EXPECT_TRUE (" #condition ")", false)
+#define ASSERT_TRUE(condition) CHECK(condition, "ASSERT_TRUE (" #condition ")", true)
+
 #define EXPECT_EQ(val1, val2) \
     do \
     { \
@@ -63,27 +86,6 @@
                       << ": ASSERT_EQ(" #val1 ", " #val2 ") failed\n" \
                       << "  Expected: " << (val2) << "\n" \
                       << "  Actual  : " << (val1) << "\n"; \
-            if (unit::current_test_failed) *unit::current_test_failed = true; \
-            return; \
-        } \
-    } while (0)
-
-#define EXPECT_TRUE(condition) \
-    do \
-    { \
-        if (!(condition)) { \
-            std::cerr << "[ FAIL ] " << __FILE__ << ":" << __LINE__ \
-                      << ": EXPECT_TRUE(" #condition ") failed\n"; \
-            if (unit::current_test_failed) *unit::current_test_failed = true; \
-        } \
-    } while (0)
-
-#define ASSERT_TRUE(condition) \
-    do \
-    { \
-        if (!(condition)) { \
-            std::cerr << "[ FAIL ] " << __FILE__ << ":" << __LINE__ \
-                      << ": ASSERT_TRUE(" #condition ") failed\n"; \
             if (unit::current_test_failed) *unit::current_test_failed = true; \
             return; \
         } \
@@ -135,3 +137,5 @@
         } \
     } while(0)
 #endif // !MACROS_H
+
+// _THROW
