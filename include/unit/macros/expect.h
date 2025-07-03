7b58cd0497e4e592c68d4ba07a444920ee5d173e
Alex <amvvec@gmail.com>
2025-07-03 21:42:47 +0300

    refactor: extract ASSERT_* and EXPECT_* macros into separate headers

--- include/unit/macros/expect.h
+++ include/unit/macros/expect.h
@@ -10,7 +10,7 @@
 #define EXPECT_FALSE(condition)                                                \
   CHECK(!(condition), "EXPECT_FALSE(", #condition ")", false)
 
-#define EXPECT_EQ(a, b) COMPARE(a, b, !=, "EXPECT_EQ", false)
+#define EXPECT_EQ(a, b) COMPARE(a, b, ==, "EXPECT_EQ", false)
 
 #define EXPECT_NE(a, b) COMPARE(a, b, !=, "EXPECT_NE", false)
 
