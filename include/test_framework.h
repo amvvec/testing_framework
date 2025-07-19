63fd06ee88513b03b38afb3b2ede80e2ac9657f1
Alex <amvvec@gmail.com>
2025-07-19 08:25:11 +0300

    refactor: Create test_framework.h with Doxygen comments, replacing unit.h

---
+++ include/test_framework.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "cli/parser.h"
+#include "core/assert.h"
+#include "core/expect.h"
+#include "core/test_macros.h"
+#include "core/test_runner.h"
+#include "mock/mock_macros.h"
